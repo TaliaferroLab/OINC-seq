5756282fbb14b97b0cd4a59856f620ba5d2fac44
Matthew Taliaferro <taliaferrojm@gmail.com>
2025-02-18 14:39:21 -0700

    add package_dir to setup

--- setup.py
+++ setup.py
@@ -7,4 +7,5 @@ author_email = 'taliaferrojm@gmail.com',
 url = 'https://github.com/TaliaferroLab/OINC-seq',
 version = '0.0.2',
 packages = find_packages(where = './src', exclude = ['workflow', 'testdata']),
+package_dir = {'':'src'},
 scripts = ['ExtractUMI.py', 'alignAndQuant.py', 'alignUMIquant.py', 'assignreads.py', 'assignreads_salmon.py', 'assignreads_salmon_ensembl.py', 'bacon_glm.py', 'conversionsPerGene.py', 'filterbam.py', 'getmismatches.py', 'getmismatches_MPRA.py', 'maskpositions.py', 'parsebamreadcount.py', 'pigpen.py', 'snps.py'])
